acb23544c13d661eb9294101abcd167752519710
B1UEN982 <selycca@gmail.com>
2025-06-21 13:57:47 +0900

    robots

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function loadChapter(id) {
     .then((data) => {
       const chapter = data.chapters.find((ch) => ch.id == id);
       if (chapter) {
-        contentDiv.textContent = chapter.content;
+        contentDiv.textContent = atob(chapter.content);
       } else {
         contentDiv.textContent = "章节未找到";
       }
